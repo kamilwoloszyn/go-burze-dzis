09e3a5e754b8cd0768a48edac8c6e8cbddbb0c0a
Kamil Wołoszyn <kamil.woloszyn1@gmail.com>
2022-06-14 21:25:05 +0200

    creating a new config

--- config/config.go
+++ config/config.go
@@ -4,3 +4,13 @@ type Config struct {
 	APIURL string `env:"API_URL"`
 	APIKey string `env:"API_KEY"`
 }
+
+func NewConfig(
+	apiUrl string,
+	apiKey string,
+) *Config {
+	return &Config{
+		APIURL: apiUrl,
+		APIKey: apiKey,
+	}
+}
